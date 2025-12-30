4e4e5da7ad5de7ffb91e9fa77b9d484ff91b0edc
Sajad CT <sajad.ct098@gmail.com>
2025-12-30 23:30:54 +0530

    Adds new routes

--- backend/internal/routes/router.go
+++ backend/internal/routes/router.go
@@ -11,40 +11,43 @@ import (
 )
 
 func SetUpRoutes(db *gorm.DB) *gin.Engine {
-
 	r := gin.Default()
 
-	r.Use(middleware.CORSMiddleware())
-	// 2. Repositories (Data Layer)
+	// Repos
+	userRepo := repository.NewUserRepository(db)
 	eventRepo := repository.NewEventRepository(db)
 	guestRepo := repository.NewGuestRepository(db)
 
-	// 3. Services (Business Logic Layer)
+	// Services
+	authService := service.NewAuthService(userRepo)
 	eventService := service.NewEventService(eventRepo, guestRepo)
 	guestService := service.NewGuestService(guestRepo, eventRepo)
 
-	// 4. Handlers (HTTP Layer)
+	// Handlers
+	authHandler := handler.NewAuthHandler(authService)
 	eventHandler := handler.NewEventHandler(eventService)
 	guestHandler := handler.NewGuestHandler(guestService)
 
-	r.GET("/hai", handler.Hello)
-
 	api := r.Group("/api")
 	{
-		// Event Routes
-		api.POST("/events", eventHandler.CreateEvent)
-		api.GET("/events", eventHandler.GetEvents)
-		// api.DELETE("/events/:id", eventHandler.DeleteEvent)
-		// api.GET("/events/:id/stats", eventHandler.GetStats)
+		api.POST("/register", authHandler.Register)
+		api.POST("/login", authHandler.Login)
 
-		// Guest Routes
-		api.POST("/invite", guestHandler.InviteGuest)
-		// api.GET("/guests/:event_id", guestHandler.GetGuests)s
-		// api.PAsTCH("/guests/:id/rsvp", guestHandler.UpdateRSVP)
-		// api.GET("/guests/details/:id", guestHandler.GetGuestDetails)
+		api.GET("/guests/details/:id", guestHandler.GetGuestDetails)
+		api.PATCH("/guests/:id/rsvp", guestHandler.UpdateRSVP)
 
-		// Export (Optional)
-		// api.GET("/events/:id/export", eventHandler.ExportGuests)
+		// Protected Routes (Host Only)
+		protected := api.Group("/")
+		protected.Use(middleware.AuthMiddleware())
+		{
+			protected.POST("/events", eventHandler.CreateEvent)
+			protected.GET("/events", eventHandler.GetEvents)
+			protected.DELETE("/events/:id", eventHandler.DeleteEvent)
+			protected.GET("/events/:id/stats", eventHandler.GetStats)
+
+			protected.POST("/invite", guestHandler.InviteGuest)
+			protected.GET("/guests/:event_id", guestHandler.GetGuests)
+		}
 	}
 
 	return r
