8da76da740fc50cb314e234d27d311e280235e62
Sajad CT <sajad.ct098@gmail.com>
2025-12-30 23:25:10 +0530

    Modifies mock repo file for test

--- backend/internal/service/mock_test.go
+++ backend/internal/service/mock_test.go
@@ -14,8 +14,8 @@ func (m *MockEventRepo) Create(event *models.Event) error {
 	return m.Called(event).Error(0)
 }
 
-func (m *MockEventRepo) GetAll() ([]models.Event, error) {
-	args := m.Called()
+func (m *MockEventRepo) GetAll(userID uint) ([]models.Event, error) {
+	args := m.Called(userID)
 	return args.Get(0).([]models.Event), args.Error(1)
 }
 
@@ -31,8 +31,6 @@ func (m *MockEventRepo) Delete(id uint) error {
 	return m.Called(id).Error(0)
 }
 
-// --- MockGuestRepo ---
-// Simulates the Guest Database
 type MockGuestRepo struct {
 	mock.Mock
 }
@@ -40,12 +38,10 @@ type MockGuestRepo struct {
 func (m *MockGuestRepo) Create(guest *models.Guest) error {
 	return m.Called(guest).Error(0)
 }
-
 func (m *MockGuestRepo) GetByEventID(id uint) ([]models.Guest, error) {
 	args := m.Called(id)
 	return args.Get(0).([]models.Guest), args.Error(1)
 }
-
 func (m *MockGuestRepo) GetByID(id uint) (*models.Guest, error) {
 	args := m.Called(id)
 	if args.Get(0) == nil {
@@ -53,11 +49,9 @@ func (m *MockGuestRepo) GetByID(id uint) (*models.Guest, error) {
 	}
 	return args.Get(0).(*models.Guest), args.Error(1)
 }
-
 func (m *MockGuestRepo) UpdateStatus(g *models.Guest, s string) error {
 	return m.Called(g, s).Error(0)
 }
-
 func (m *MockGuestRepo) GetGuestWithEvent(id uint) (*models.Guest, error) {
 	args := m.Called(id)
 	if args.Get(0) == nil {
@@ -65,8 +59,31 @@ func (m *MockGuestRepo) GetGuestWithEvent(id uint) (*models.Guest, error) {
 	}
 	return args.Get(0).(*models.Guest), args.Error(1)
 }
-
 func (m *MockGuestRepo) CountByStatus(eventID uint, status string) (int64, error) {
 	args := m.Called(eventID, status)
 	return int64(args.Int(0)), args.Error(1)
+}
+
+type MockUserRepo struct {
+	mock.Mock
+}
+
+func (m *MockUserRepo) Create(user *models.User) error {
+	return m.Called(user).Error(0)
+}
+
+func (m *MockUserRepo) GetByEmail(email string) (*models.User, error) {
+	args := m.Called(email)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*models.User), args.Error(1)
+}
+
+func (m *MockUserRepo) GetByID(id uint) (*models.User, error) {
+	args := m.Called(id)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*models.User), args.Error(1)
 }
