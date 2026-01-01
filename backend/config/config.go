0a72b01653d73c1c716a0c17b5b8e92eb5a1ba14
Sajad CT <sajad.ct098@gmail.com>
2026-01-01 17:42:44 +0530

    Modifies config files

--- backend/config/config.go
+++ backend/config/config.go
@@ -14,8 +14,6 @@ type DatabaseConfig struct {
 	User     string
 	Password string
 	Name     string
-	SSLMode  string
-	TimeZone string
 }
 
 func LoadDatabaseConfig() string {
@@ -30,19 +28,11 @@ func LoadDatabaseConfig() string {
 		User:     os.Getenv("DB_USER"),
 		Password: os.Getenv("DB_PASSWORD"),
 		Name:     os.Getenv("DB_NAME"),
-		SSLMode:  os.Getenv("DB_SSLMODE"),
-		TimeZone: os.Getenv("DB_TIMEZONE"),
 	}
 
 	return fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
-		cfg.Host,
-		cfg.User,
-		cfg.Password,
-		cfg.Name,
-		cfg.Port,
-		cfg.SSLMode,
-		cfg.TimeZone,
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.Name,
 	)
 
 }
